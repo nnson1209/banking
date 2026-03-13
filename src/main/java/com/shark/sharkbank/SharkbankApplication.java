ea30d8478ef72df799df40e459bd2bce4502ed70
nnson1209 <nnson120924@gmail.com>
2026-03-13 12:04:57 +0700

    connecting DB

---
+++ src/main/java/com/shark/sharkbank/SharkbankApplication.java
@@ -0,0 +1,15 @@
+package com.shark.sharkbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkbankApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SharkbankApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/shark/sharkbank/SharkbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shark.sharkbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
